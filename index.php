9bd89b7597148715d8dd268d38500085b6daf335
muneeb <muneebsaddal@github.com>
2018-04-09 22:13:55 +0500

    header main page layout done

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet"> 
+</head>
+
+<body class="top">
+<div class="container">
+<div class="wrapper row">
+	<div class="col-12 row topbar">
+		<div class="col-2 logo">
+			<a href="">Logo</a>
+		</div>
+		<div class="col-9 nav-items">
+			<ul class="top-nav">
+				<li class="nav-item">Contact</li>
+				<li class="nav-item">About</li>
+				<li class="nav-item">Home</li>
+			</ul>
+		</div>
+	</div>
+	<div class="col-12 row main-center">
+		Your Favorite Source of Free Bootstrap Themes<br><hr>
+	</div>
+	<div class="col-12 row btn-center">
+		<!--<a class="btn btn-xl btn-primary" href="">Community</a>
+		<a class="btn btn-xl btn-primary" href="">Find out more</a>
+		<a class="btn btn-xl btn-primary" href="">Shop</a>-->
+		<button class="btn btn-xl btn-primary">Community</button>
+		<button class="btn btn-xl btn-primary">Find out more</button>
+		<button class="btn btn-xl btn-primary">Shop</button>
+	</div>
+</div>
+<br>
+<div class="col-12 row heading">
+	<h1>Latest</h1>
+</div>
+
+<div class="col-9 articles">
+	test
+</div>
+</div>
+</body>
+</html>
